a6eea57213448dfbfe737e568af8359703a6fa52
ARDelarosa <63564596+ARDelarosa@users.noreply.github.com>
2024-04-29 21:28:03 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Create a msg to let the user know they have been given the password
+console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+
+// Create a variable for each of the three numbers
+let num1 = 5 + 5
+let num2 = 80 / 2
+let num3 = 3 * 13
+
+// Creating a variable that prints out the password with a msg
+let total = `The password is ${num1} - ${num2} - ${num3}`
+
+//Print out the password and use a dialog box alert too
+console.log(total)
+
+alert(total)
